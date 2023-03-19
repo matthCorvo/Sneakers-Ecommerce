10bb6bc2b16fe8eb66dd74b6e53b6481f20c64bb
matthieu Corvo <76264628+matthCorvo@users.noreply.github.com>
2023-03-19 18:24:59 +0100

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
     return (
         <div className="banner">
         <div className="banner-text">
-          <h1>25% de remise sur vos article favoris</h1>
+          <h1>25% de remise sur vos articles favoris</h1>
           <h3>Réservé aux Membres Nike. Entrez le code BTS21 lors du paiement.*</h3>
           {/* <div className="d-flex justify-content-center mt-4">
           <Link to="/">
